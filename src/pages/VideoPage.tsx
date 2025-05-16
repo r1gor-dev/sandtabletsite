e8c8968a0ef903d7784b351cdd9209911016e9b1
r1gor-dev <r1gor@bk.ru>
2025-05-16 04:22:20 +0300

    video ok size

--- src/pages/VideoPage.tsx
+++ src/pages/VideoPage.tsx
@@ -5,7 +5,7 @@ const VideoPage = () => (
     <h2 className="text-center mb-4">Видео показа функционала на этапах разработки</h2>
     <div className="d-flex justify-content-center">
       <video controls style={{ maxWidth: '100%', borderRadius: '10px', boxShadow: '0 0 15px rgba(0,0,0,0.2)' }}>
-        <source src="/video/projectvideo.mp4" type="video/mp4" />
+        <source src="/video/prjectvideo.mp4" type="video/mp4" />
         Ваш браузер не поддерживает видео.
       </video>
     </div>
